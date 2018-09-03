2b05375f3482ee658d29cd06167f5d0bf45da092
petermeissner <retep.meissner@gmail.com>
2018-09-03 17:13:13 +0200

    scripts: page view downloads

---
+++ scripts/view_download_2014.R
@@ -0,0 +1,18 @@
+# script to download all data for 2014
+library(wpd)
+
+wpd_download_month(year = 2014, month = 1)
+wpd_download_month(year = 2014, month = 2)
+wpd_download_month(year = 2014, month = 3)
+wpd_download_month(year = 2014, month = 4)
+wpd_download_month(year = 2014, month = 5)
+wpd_download_month(year = 2014, month = 6)
+wpd_download_month(year = 2014, month = 7)
+wpd_download_month(year = 2014, month = 8)
+wpd_download_month(year = 2014, month = 9)
+wpd_download_month(year = 2014, month = 10)
+wpd_download_month(year = 2014, month = 11)
+wpd_download_month(year = 2014, month = 12)
+
+
+
