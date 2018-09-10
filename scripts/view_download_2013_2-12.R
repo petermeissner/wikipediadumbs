5c059e713cb6ed023def306ff79c244d282d2ed7
petermeissner <retep.meissner@gmail.com>
2018-09-10 21:18:15 +0200

    minor: 2013 download

---
+++ scripts/view_download_2013_2-12.R
@@ -0,0 +1,18 @@
+# script to download all data for 2013
+library(wpd)
+
+#wpd_download_month(year = 2013, month = 2)
+#wpd_download_month(year = 2013, month = 3)
+#wpd_download_month(year = 2013, month = 4)
+#wpd_download_month(year = 2013, month = 5)
+#wpd_download_month(year = 2013, month = 6)
+wpd_download_month(year = 2013, month = 7)
+wpd_download_month(year = 2013, month = 8)
+wpd_download_month(year = 2013, month = 9)
+wpd_download_month(year = 2013, month = 10)
+wpd_download_month(year = 2013, month = 11)
+wpd_download_month(year = 2013, month = 12)
+
+
+
+
