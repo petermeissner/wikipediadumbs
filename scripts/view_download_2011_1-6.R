d52175d3e942472e17c6b3fbd6690830809997c9
petermeissner <retep.meissner@gmail.com>
2018-09-17 10:27:14 +0200

    2011

---
+++ scripts/view_download_2011_1-6.R
@@ -0,0 +1,14 @@
+# script to download all data for 2011
+library(wpd)
+
+wpd_download_month(year = 2011, month = 1)
+wpd_download_month(year = 2011, month = 2)
+wpd_download_month(year = 2011, month = 3)
+wpd_download_month(year = 2011, month = 4)
+wpd_download_month(year = 2011, month = 5)
+wpd_download_month(year = 2011, month = 6)
+
+
+
+
+
---
+++ scripts/view_download_2011_7-12.R
@@ -0,0 +1,14 @@
+# script to download all data for 2011
+library(wpd)
+
+wpd_download_month(year = 2011, month = 7)
+wpd_download_month(year = 2011, month = 8)
+wpd_download_month(year = 2011, month = 9)
+wpd_download_month(year = 2011, month = 10)
+wpd_download_month(year = 2011, month = 11)
+wpd_download_month(year = 2011, month = 12)
+
+
+
+
+
