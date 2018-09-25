e25649c44f34ca3018ff6d2af8a480681d727ae6
petermeissner <retep.meissner@gmail.com>
2018-09-25 08:31:28 +0200

    minor: cleanup

--- scripts/wpd_dashboard_progress.R
+++ scripts/wpd_dashboard_progress.R
@@ -21,41 +21,6 @@ tasks %>%
   geom_hline(data = data.frame(y=0), aes(yintercept=y))
 
 
-#
-tasks_done <-
-  tasks %>%
-  group_by(task_date) %>%
-  summarise(
-    sum_done = sum(task_status == 'done')
-  ) %>%
-  filter(
-    sum_done == 1
-  ) %>%
-  left_join(
-   tasks, by="task_date"
-  ) %>%
-  group_by(task_date) %>%
-  summarise(
-    sum_progress = sum(task_volume, na.rm = TRUE)/20,
-    sum_duration = sum(task_duration, na.rm = TRUE)/20,
-    ts_update    = max(task_status_ts)
-  ) %>%
-  left_join(
-    (task %>% select(task_date, task_id)), by = "task_date"
-  )
-
-
-
-update <-
-  wpd_task_update(
-    task_id        = tasks_done$task_id,
-    task_status    = "done",
-    task_duration  = tasks_done$sum_duration,
-    task_volume    = tasks_done$sum_progress,
-    task_status_ts = tasks_done$ts_update
-  )
-
-
 
 
 
