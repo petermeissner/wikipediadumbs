54f9fd191c70eb60d55276e38efdcf319844d4c9
petermeissner <retep.meissner@gmail.com>
2018-10-08 08:26:34 +0200

    db import

--- scripts/db_import_2013.R
+++ scripts/db_import_2013.R
@@ -1,14 +1,19 @@
 
 
 # packages
-
 library(wpd)
 library(dplyr)
+library(future)
+library(future.apply)
 
+
+# options
+plan("multisession", workers = availableCores())
 import_date <- "2013"
 
-# get distribution of jobs
 
+
+# get distribution of jobs
 topology <- wpd_db_topology()
 
 
@@ -46,51 +51,66 @@ sql <-
     page_view_date date NULL,
     page_view_count int4 NULL
   )
-  ;
-  ",
+    ;
+    ",
     wpd_languages,
     import_date
-  )
+    )
 for(i in seq_along(sql)) wpd_get_query_master(sql[i])
 
 
-
-start <- Sys.time()
+sql_list <- list()
 for( i in seq_len(nrow(topo)) ){
-  sql <-
-    wpd_sql(
-      "insert into page_views_%s_%s_import
-    select page_id, page_view_date, sum(page_view_count) from
-    dblink(
-      'dbname=wikipedia port=5432 host=%s user=%s password=%s',
-      'select * from page_views_%s where page_view_date = ''%s''::date')
-      as dings(page_id int4, page_view_date date, page_view_count int4)
-    group by page_id, page_view_date
-    ;
-    ",
-      wpd_languages,
-      import_date,
-      wpd_nodes[topo$node[i]],
-      Sys.getenv("wpd_user"),
-      Sys.getenv("wpd_password"),
-      wpd_languages,
-      topo$task_date[i]
-    )
 
-  cat("\n", topo$node[i], "-", as.character(topo$task_date[i]), ": ")
-  for ( k in seq_along(wpd_languages) ) {
-    cat(" ", wpd_languages[k], sep="")
-    wpd_get_query_master(sql[k])
+  if( wpd_nodes[topo$node[i]] == Sys.info()["nodename"]){
+    sql <-
+      wpd_sql(
+        "insert into page_views_%s_%s_import
+        select page_id, page_view_date, sum(page_view_count)
+        from page_views_%s where page_view_date = '%s'::date
+        group by page_id, page_view_date
+        ;
+        ",
+        wpd_languages,
+        import_date,
+        wpd_languages,
+        topo$task_date[i]
+      )
+  }else{
+    sql <-
+      wpd_sql(
+        "insert into page_views_%s_%s_import
+        select page_id, page_view_date, sum(page_view_count) from
+        dblink(
+        'dbname=wikipedia port=5432 host=%s user=%s password=%s',
+        'select * from page_views_%s where page_view_date = ''%s''::date')
+        as dings(page_id int4, page_view_date date, page_view_count int4)
+        group by page_id, page_view_date
+        ;
+        ",
+        wpd_languages,
+        import_date,
+        wpd_nodes[topo$node[i]],
+        Sys.getenv("wpd_user"),
+        Sys.getenv("wpd_password"),
+        wpd_languages,
+        topo$task_date[i]
+      )
   }
-  cat(
-    "\n", i, "/",
-    nrow(topo), "in",
-    as.character(hms::as.hms(round(difftime(Sys.time(), start, units = "secs"))))
-  )
 
+  sql_list[[ length(sql_list) + 1 ]] <-
+    data_frame(
+      page_view_date = topo$task_date[i],
+      page_view_lang = wpd_languages,
+      node           = topo$node[i],
+      sql            = sql
+    )
 }
 
 
+sql_df      <- do.call(rbind, sql_list)
+sql_df      <- sample_n(sql_df, nrow(sql_df))
+sql_df_list <- split(sql_df, seq_len(nrow(sql_df)))
 
 
 
@@ -98,7 +118,114 @@ for( i in seq_len(nrow(topo)) ){
 
 
 
+results <-
+  do.call(
+    rbind,
+    future_lapply(
+      X   = sql_df_list,
+      FUN =
+        function(df){
 
+
+          jobs <-
+            wpd_get_query_master(
+              wpd_sql(
+                "select * from import_jobs
+                where page_view_date ='%s'::date and
+                page_view_lang = '%s'
+                ",
+                df$page_view_date,
+                df$page_view_lang
+              )
+            )$return
+
+          if ( nrow(jobs) == 0 ){
+            wpd_get_query_master(
+              wpd_sql(
+                "insert into import_jobs (page_view_lang, page_view_date)
+                values ('%s', '%s')",
+                df$page_view_lang,
+                df$page_view_date
+              )
+            )
+            jobs <-
+              wpd_get_query_master(
+                wpd_sql(
+                  "select * from import_jobs
+                  where page_view_date ='%s'::date and
+                  page_view_lang = '%s'
+                  ",
+                  df$page_view_date,
+                  df$page_view_lang
+                )
+              )$return
+          }
+
+          if ( jobs$import_status %in% c("start", "done") ){
+
+            message("done already: ", df$page_view_date, " ", df$page_view_lang)
+            df$sql    <- NULL
+            df$status <- "done already"
+            return(df)
+
+          } else {
+
+            message(
+              "started:   ", df$page_view_date, " ", df$page_view_lang,
+              " -- ", as.character(Sys.time()), " -- start"
+            )
+
+            if ( jobs$import_status %in% "error" ) {
+              wpd_get_query_master(
+                wpd_sql(
+                  "delete from page_views_%s_%s_import where page_view_date = '%s'::date",
+                  df$page_view_lang,
+                  import_date,
+                  df$page_view_date
+                )
+              )
+            }
+
+            wpd_get_query_master(
+              wpd_sql(
+                "update import_jobs set import_status = 'start'
+                where page_view_date = '%s'::date and page_view_lang = '%s'",
+                df$page_view_date,
+                df$page_view_lang
+              )
+            )
+
+            res <- wpd_get_query_master(df$sql)
+            if ( res$status$errorMsg == "OK" ){
+              status <- "done"
+            }else{
+              status <- "error"
+            }
+
+
+            wpd_get_query_master(
+              wpd_sql(
+                "update import_jobs set import_status = '%s'
+                where page_view_date = '%s'::date and page_view_lang = '%s'",
+                status,
+                df$page_view_date,
+                df$page_view_lang
+              )
+            )
+
+            message(
+              "processed: ", df$page_view_date, " ", df$page_view_lang,
+              " -- ", as.character(Sys.time()), " -- ",status
+            )
+            df$sql    <- NULL
+            df$status <- status
+            return(df)
+          }
+
+        }
+                )
+              )
+results
 
 
 
