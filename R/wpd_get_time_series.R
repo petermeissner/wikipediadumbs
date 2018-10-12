719ff1c3121b26bb60af9b1cfe2c0a6bddd71ef6
petermeissner <retep.meissner@gmail.com>
2018-10-12 14:38:01 +0200

    wpd_get_time_series()

--- R/wpd_get_time_series.R
+++ R/wpd_get_time_series.R
@@ -5,55 +5,64 @@
 #'
 #' @export
 #'
-wpd_get_time_series <- function(page = NULL, lang = NULL){
+wpd_get_time_series <- function(page = NULL, lang = NULL, regex = NULL){
+
+  # check input
+  if ( !is.null(regex) & !is.null(page) ){
+    stop("You cannot use page name and regex argument at the same time.")
+  }
+
+  stopifnot(!is.null(lang))
 
   # generate query
-  sql <-
-    wpd_sql(
-      "
-      select
-        a.page_name,
-        a.page_id,
-        sum(b.page_view_count),
-        b.page_view_date
-      from dict_%s as a
-      left join
-	      page_views_%s as b
-        on a.page_id=b.page_id
-	    where page_name = '%s'
-	    group by (a.page_name, a.page_id, b.page_view_date)
-	    order by a.page_name, page_view_date
-    ;
-    ",
-      lang,
-      lang,
-      page
+  if ( !is.null(page) ){
+    sql <-
+      wpd_sql(
+        "select a.page_id, a.page_name, b.year, b.page_views
+  from (select * from dict_%s where page_name = '%s') as a
+  left join imports_%s as b on a.page_id = b.page_id
+  ;
+",
+        lang,
+        page,
+        lang
+      )
+
+  } else if ( !is.null(regex) ){
+    sql <-
+      wpd_sql(
+        "select a.page_id, a.page_name, b.year, b.page_views
+  from (select * from dict_%s where page_name ~* '%s') as a
+  left join imports_%s as b on a.page_id = b.page_id
+  ;",
+        lang,
+        regex,
+        lang
+      )
+  } else {
+    stop("Either page or regex have to be specified")
+  }
+
+  con <- wpd_connect_master()
+  on.exit(DBI::dbDisconnect(con))
+
+  res <-
+    wpd_get_queries(
+      queries = sql,
+      con     = con,
+      flatten = FALSE
     )
 
-  # execute query
-  res <- list()
+  res_dfs <-
+    lapply(res, `[[`, "return")
 
-  for( node in names(wpd_nodes) ){
-    cat(".")
-    con <- wpd_connect(node = node)
-    res <-
-      c(
-        res,
-        wpd_get_queries(
-          sql,
-          con     = con,
-          flatten = FALSE,
-          verbose = FALSE
-        )
-      )
-    DBI::dbDisconnect(conn = con)
-  }
-  cat("\n")
+
+
 
 
   # rbind and return
-  res_flat <- data.table::rbindlist(res, fill = TRUE)
-  res_flat[!is.na(res_flat$page_view_date),]
+  res_dfs_flat <- data.table::rbindlist(res_dfs, fill = TRUE)
+  res_dfs_flat
 }
 
 
