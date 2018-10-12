c52c5f082869b468ad79dbcb90cf2e077e0ea7b8
petermeissner <retep.meissner@gmail.com>
2018-10-12 14:38:19 +0200

    wpd_dashboard_progress

--- scripts/wpd_dashboard_progress.R
+++ scripts/wpd_dashboard_progress.R
@@ -15,13 +15,10 @@ jobs <-
 	where job_status = 'start'")$return %>%
   data.frame() %>%
   rbind(data.frame(job_id = integer(), job_run_node = character(), task_date = integer())) %>%
-  dplyr::group_by(
-    job_id, job_run_node
+  group_by(
+    job_run_node, task_date
   ) %>%
-  dplyr::summarise(
-    task_date = min(task_date)
-  ) %>%
-  arrange(job_run_node)
+  summarise()
 
 jobs$y <- scales::rescale(seq_len(nrow(jobs)), from = c(1, nrow(jobs)), to =c(0,20))
 
@@ -36,11 +33,13 @@ plot <-
     task_status   = sum(task_status == "done"),
     hours = mean(task_duration/3600 , na.rm = TRUE)
   ) %>%
-  ggplot(aes(x = task_date, y = task_status, color = hours)) +
-  geom_col(width=0.005) +
+  ggplot(aes(x = task_date, y = task_status)) +
+  geom_col(width = 2) +
   theme_bw() +
-  geom_hline(data = data.frame(y=0), aes(yintercept=y))+
-  geom_vline(data = jobs, aes(xintercept = task_date, color = job_run_node))
+  geom_vline(
+    data = jobs,
+    aes(xintercept = task_date, color = job_run_node)
+  )
 
 if(nrow(jobs) > 0 ){
   plot <-
@@ -53,7 +52,7 @@ plot
 
 
 # print missing dates
-missing <- tasks %>% filter(task_status!="done", task_date >= "2012-01-01")
+missing <- tasks %>% filter(task_status!="done")
 missing$task_date %>% unique() %>% sort() %>% as.character()
 
 
