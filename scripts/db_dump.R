302bff3428fc7d27f743c1513c20bbf802a7da13
petermeissner <retep.meissner@gmail.com>
2018-11-11 10:17:49 +0100

    db_dump: make exports for postgres and for compatibility sql

--- scripts/db_dump.R
+++ scripts/db_dump.R
@@ -13,10 +13,12 @@ tables <- sample(tables)
 setwd("/data/wpd/")
 start = Sys.time()
 
+system("pg_dump --schema-only -d wikipedia > wikipedia_schema.sql")
+
 for(i in seq_along(tables)){
   dbt_hlp_progress(i = i, ii = length(tables), start = start, m = tables[i])
-  system(sprintf("pg_dump --data-only --column-inserts -d wikipedia -t %s | gzip > %s.gz", tables[i], tables[i]))
-  system(sprintf("pg_dump --schema-only -d wikipedia -t %s > %s.sql", tables[i], tables[i]))
+  system(sprintf("pg_dump --data-only -d wikipedia -t %s | gzip > %s_copy.gz", tables[i], tables[i]))
+  system(sprintf("pg_dump --data-only --column-inserts -d wikipedia -t %s | gzip > %s_inserts.gz", tables[i], tables[i]))
 }
 
 
