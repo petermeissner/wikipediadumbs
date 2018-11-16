827144e28feb2023f9ac1587b48fb44dda96b1b7
petermeissner <retep.meissner@gmail.com>
2018-11-16 23:43:45 +0100

    copy postgrres db to sqlite

--- scripts/db_copy_to_sqlite.R
+++ scripts/db_copy_to_sqlite.R
@@ -2,75 +2,46 @@ library(wpd)
 library(DBI)
 library(RSQLite)
 
+getwd()
 
 
-
-copy_table <-
-  function(table_name){
-
-    # get current time
-    start    <- Sys.time()
-
-    # open up connections
-    con_from <- wpd_connect_master()
-    con_to   <- dbConnect(SQLite(), "f:/test.db")
-
-    # ensure closing of connections
-    on.exit({
-      dbDisconnect(con_from)
-      dbDisconnect(con_to)
-    })
-
-    # query size of table
-    statement_size <-
-      dbGetQuery(
-        con_from,
-        wpd_sql("select count(*) from %s", table_name)
-      )$count
-
-    # send query
-    statement_exec <-
-      dbSendQuery(
-        con_from,
-        wpd_sql("select * from %s", table_name)
-      )
+# open up connections
+con_from <- wpd_connect_master()
+con_to   <- dbConnect(SQLite(), "wpd.db")
 
 
-    # fetch first batch of results
-    statement_res  <- dbFetch(statement_exec, n = 10000)
-    size           <- nrow(statement_res)
+# get list of tables
+table_names <-
+  grep(
+    x       = sort(dbListTables(con_from)),
+    pattern = "(^dict_)|(^page_views_daily)|(^upload)",
+    value   = TRUE
+  )
+
+# ensure closing of connections
+dbDisconnect(con_from)
+dbDisconnect(con_to)
 
 
-    # ensure table exists at to table
-    if( dbExistsTable(con_to, table_name) ){
-      # do nothing
-    } else {
-      if ( size > 0 ){
-        dbCreateTable(con_to, table_name, statement_res)
-      }
-    }
+# loop over list of tables
+RES <- list()
+for ( i in seq_along(table_names) ){
+  # open up connections
+  con_from <- wpd_connect_master()
+  con_to   <- dbConnect(SQLite(), "wpd.db")
 
-    # loop through
-    while ( nrow(statement_res) > 0 ){
+  # copy table
+  RES[[ length(RES) + 1 ]] <-
+    db_copy_table_to_db(
+      table_name = table_names[i],
+      con_from   = con_from,
+      con_to     = con_to,
+      batch_size = 100000
+    )
 
-      # check encoding
-      for ( i in seq_len(ncol(statement_res)) ){
-        if ( class(statement_res[[i]]) %in% "character" ){
-          Encoding(statement_res[[i]]) <- "UTF-8"
-        }
-      }
-
-      size <- nrow(statement_res) + size
-      dbt_hlp_progress(size, statement_size, start = start, m = table_name)
-      dbAppendTable(con_to, table_name, value = statement_res)
-
-      statement_res <- dbFetch(statement_exec, n = 10000)
-    }
-
-    return(list(start = start, duration = hms::hms(as.integer(Sys.time() - start))))
-  }
-
-
-copy_table("dict_it")
+  # ensure closing of connections
+  dbDisconnect(con_from)
+  dbDisconnect(con_to)
+}
 
 
